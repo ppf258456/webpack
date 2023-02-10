4856dee252368230a39d26a48e6a2776edd42e17
ppf258456 <916861792@qq.com>
2023-02-10 16:08:21 +0800

    第一次提交

---
+++ demo01/src/m1.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+export default{
+    setH1(){
+        // document.body.insertAdjacentHTML("beforeend","<h3>哈哈哈哈</h3>")
+        $("body").append("<h1>哈哈</h1>")
+    }
+}
---
+++ demo03/webpack.config.js
@@ -0,0 +1,4 @@
+module.exports={
+    //设置打包模式 production：生产模式 development 开发模式
+    mode:"production",
+}
