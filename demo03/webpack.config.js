0ad86a8735ce973842ca59b591946b06d241abda
ppf258456 <916861792@qq.com>
2023-02-10 16:23:24 +0800

    entry属性

--- demo03/webpack.config.js
+++ demo03/webpack.config.js
@@ -1,4 +1,11 @@
 module.exports={
     //设置打包模式 production：生产模式 development 开发模式
     mode:"production",
+    //用来指定打包时的主文件 默认为./src/index.js
+    //entry:"./hello/hello.js",
+    //entry:["./src/a.js","./src/b.js"]
+    entry:{
+        a:"./src/a.js",
+        b:"./src/b.js",
+    }
 }
